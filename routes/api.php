c8d88dde44e32c8861f591e21c765cee8ea0b90f
prashan0912 <prashantsahu0912@gmail.com>
2026-02-09 12:01:53 +0530

    installed api folder

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get("/data", function () {
-    return ["name" => "prashant", "age" => 24];
-});
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
